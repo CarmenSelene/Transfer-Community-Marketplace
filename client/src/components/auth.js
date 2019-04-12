19a8b36325867252733392ad666d182ad452215f
Pavani Vithala <pavani.vithala18@gmail.com>
2019-04-11 21:26:42 -0400

    Working on Auth

--- client/src/components/auth.js
+++ client/src/components/auth.js
@@ -1,12 +1,36 @@
+import API from "../../utils/API";
 class Auth {
     constructor() {
         this.authenticated = false;
     }
 
     login(cb) {
-// auth goes here
-        this.authenticated = true;
-        cb();
+        let usercheck = {
+            email: "pavani.vithala@gmail.com",
+            password: "Manu"
+        }
+        API.loginUser(usercheck)
+        .then(res => {
+            if (res.data.status === "error") {
+                alert("Check your user credentials");
+                this.authenticated = false;
+                throw new Error(res.data.message);
+            }else{
+                console.log("user Exists and login Successful");
+           // this.setState({ auth: true, currentUser: res.data._id })
+            this.authenticated = true;
+            }
+            
+        })
+        .then(() => {
+            //let currUser = this.state.currentUser;
+            console.log("authenticated is  ", +this.authenticated);
+            
+
+        })
+        .catch(err => console.log(err)); 
+        
+       // cb();
     }
 
     logout(cb) {
